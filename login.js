096fddec9aafbd7867aff24020e8838a29b04c0d
Mimo <mimo@gmail.com>
2019-07-24 16:08:26 +0200

    finis

---
+++ login.js
@@ -0,0 +1,21 @@
+function aled (){
+	let mail = $("#email");
+	let mdp = $("#password");
+	
+
+	$.ajax({
+		url: "http://192.168.1.112:8001/login",
+		type: 'POST',
+		data: {
+			email: mail.val(), 
+			password: mdp.val()
+		},
+		success: function(result){
+			console.log(result);
+			alert(result.message);
+		},
+		error: function(){
+			alert("Incorrect");
+		},
+	});
+}
